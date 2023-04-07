6be7fd8d3e5d63230cd4ec69481f573ba7fd1a0f
Yusra5amir <125461837+Yusra5amir@users.noreply.github.com>
2023-04-08 00:30:13 +0500

    final zakat calculator

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+alert ("Zakat Calculator");
+var zakatPercentage = 0.025;
+var userInput = (prompt("Enter Your Amount"));
+var result = (zakatPercentage) * (userInput);
+alert ("Your Zakat Value is" + result);
